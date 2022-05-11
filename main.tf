4523311052093063dc8c5bddbf6d696634785243
Ben Northway <northben@pixelchef.net>
2022-05-11 14:34:15 -0600

    add load balancer, trello webhooks are being indexed

--- main.tf
+++ main.tf
@@ -25,6 +25,14 @@ resource "aws_ecs_task_definition" "trunk" {
       cpu       = 1024
       memory    = 2048
       essential = true
+      portMappings = [
+        {
+          containerPort = 8000
+        },
+        {
+          containerPort = 9000
+        }
+      ]
       logConfiguration = {
         logDriver = "awslogs",
         options = {
@@ -43,6 +51,14 @@ resource "aws_ecs_task_definition" "trunk" {
 resource "aws_ecs_cluster" "trunk" {
   name               = "trunk"
   capacity_providers = ["FARGATE"]
+  configuration {
+    execute_command_configuration {
+      logging = "OVERRIDE"
+      log_configuration {
+        cloud_watch_log_group_name = "trunk"
+      }
+    }
+  }
 }
 resource "aws_ecs_service" "trunk" {
   name                 = "trunk"
@@ -52,8 +68,18 @@ resource "aws_ecs_service" "trunk" {
   launch_type          = "FARGATE"
   force_new_deployment = true
   network_configuration {
-    subnets          = [aws_subnet.trunk.id]
+    subnets          = [aws_subnet.trunk_subnet1.id, aws_subnet.trunk_subnet2.id]
     assign_public_ip = true
     security_groups  = [aws_security_group.allow_splunk.id]
+  }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.splunk_target.arn
+    container_name   = "splunk"
+    container_port   = 8000
+  }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.webhook_target.arn
+    container_name   = "splunk"
+    container_port   = 9000
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,71 @@
-resource "aws_subnet" "trunk" {
+resource "aws_subnet" "trunk_subnet1" {
   vpc_id     = var.vpc_id
-  cidr_block = var.subnet_cidr
+  cidr_block = var.subnet1_cidr
   tags = {
-    Name = "trunk"
+    Name = "trunk 1"
+  }
+}
+resource "aws_subnet" "trunk_subnet2" {
+  vpc_id     = var.vpc_id
+  cidr_block = var.subnet2_cidr
+  tags = {
+    Name = "trunk 2"
+  }
+}
+
+resource "aws_lb" "splunk_lb" {
+  name               = "splunk"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_splunk.id]
+  subnets            = [aws_subnet.trunk_subnet1.id, aws_subnet.trunk_subnet2.id]
+}
+resource "aws_lb_target_group" "splunk_target" {
+  name        = "splunk"
+  port        = 8000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+  health_check {
+    path = "/en-US/account/login"
+  }
+}
+resource "aws_lb_listener" "splunk_listener" {
+  load_balancer_arn = aws_lb.splunk_lb.arn
+  port              = "8000"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.splunk_target.arn
+  }
+}
+resource "aws_lb_target_group" "webhook_target" {
+  name        = "webhook"
+  port        = 9000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+  health_check {
+    path = "/trello"
+  }
+}
+resource "aws_lb_listener" "webhook_listener" {
+  load_balancer_arn = aws_lb.splunk_lb.arn
+  port              = "9000"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.webhook_target.arn
+  }
+}
+
+resource "aws_route53_record" "trunk" {
+  zone_id = var.route53_zone
+  name    = var.trunk_dns_name
+  type    = "A"
+  alias {
+    name                   = aws_lb.splunk_lb.dns_name
+    zone_id                = aws_lb.splunk_lb.zone_id
+    evaluate_target_health = false
   }
 }
